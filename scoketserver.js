92fbf016b5bbc912aa75e44d4d97cb3ea2c93ade
xinshijiedemaoxian <xia_duanjian!163.com>
2016-11-09 17:02:51 +0800

    文件夹改名

--- scoketserver.js
+++ scoketserver.js
@@ -7,7 +7,6 @@ module.exports.listen=function(httpServer){
     //让socketio模块监听httpServer(绑定)
     var io=socketio.listen(httpServer);   //io理解为10086
 
-
 //服务器端会监听客户端的socket连接请求---会触发一个名为connection
 //每当来一个客户端请求，服务端就会创建一个socket，由该socket负责
     io.on("connection",function(socket){
@@ -34,7 +33,6 @@ module.exports.listen=function(httpServer){
                 text: "1:" +data.comName+ "\r\n2:" +data.idenF+ "\r\n3:"+data.adrPhone+ "\r\n4:"+data.bankCon  // 文本
                 //html: "<p>测试002</p>"
             };
-
             transporter.sendMail(mailOptions, function (err, info) {
                 if (err) {
                     console.log(err);
